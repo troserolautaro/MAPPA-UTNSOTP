acb638cfc47a4bd2aca08e0d6883cf9c6e986cb5
Elias Lischinsky <elischinsky@frba.utn.edu.ar>
2023-09-15 13:01:54 +0000

    El FileSystem ahora ler su archivo de configuración

--- FileSystem/src/FileSystem.c
+++ FileSystem/src/FileSystem.c
@@ -2,7 +2,15 @@
 
 int main(void) {
 
-	logger = log_create("log.log", "Servidor", 1, LOG_LEVEL_DEBUG);
+	t_log *logger = log_create("log.log", "Servidor", 1, LOG_LEVEL_DEBUG);
+
+	t_config* config;
+	char* ipMemoria,* ipFileSystem ;
+	char* puertoEscucha,* puertoMemoria,* puertoFileSystem ;
+	char* path_fat, * path_bloques, * path_fcb;
+	char* cant_bloques_total,* cant_bloques_swap;
+	char* tam_bloque;
+	char* retardo_acceso_bloque,* retardo_acceso_fat;
 
 	int server_fd = iniciar_servidor();
 	log_info(logger, "Servidor listo para recibir al cliente");
@@ -28,7 +36,19 @@ int main(void) {
 					break;
 				}
 			}
-
+	//CONFIGURACION DE CPU /*
+		//CONFIGURACION DE FILESYSTEM
+		ipMemoria = config_get_string_value(config,"IP_MEMORIA");
+		puertoMemoria = config_get_string_value(config,"PUERTO_MEMORIA");
+		puertoEscucha = config_get_string_value(config,"PUERTO_ESCUCHA");
+		path_fat = config_get_string_value(config,"PATH_FATH");
+		path_bloques = config_get_string_value(config,"PATH_BLOQUES");
+		path_fcb = config_get_string_value(config,"PATH_FCB");
+		cant_bloques_total = config_get_string_value(config,"CANT_BLOQUES_TOTAL");
+		cant_bloques_swap = config_get_string_value(config,"CANT_BLOQUES_SWAP");
+		tam_bloque = config_get_string_value(config,"TAM_BLOQUE");
+		retardo_acceso_bloque = config_get_string_value(config,"RETARDO_ACCESO_BLOQUE");
+		retardo_acceso_fat = config_get_string_value(config,"RETARDO_ACCESO_FAT");
 
 	return EXIT_SUCCESS;
 }
