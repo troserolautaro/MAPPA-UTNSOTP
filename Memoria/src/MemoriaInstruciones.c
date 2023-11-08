ba8fefd4918e0be67f83c111988a1f341eede4c8
juan9772 <juanpirinto@hotmail.com>
2023-11-08 02:22:49 +0000

    correcion del merge
    se pasa a cargarinstrucciones.c la funcion cargar instrucciones de la rama fifo

--- Memoria/src/MemoriaInstruciones.c
+++ Memoria/src/MemoriaInstruciones.c
@@ -2,16 +2,16 @@
 char * pathInstrucciones;
 t_list* cargar_instrucciones(char** file){
 	FILE * fileInstrucciones;
-		t_list *listaInstrucciones;
-		listaInstrucciones= list_create();
+		t_list *listaInstrucciones = list_create();
 		char * direccionIns=string_new();
 		//une la ruta de los archivos, con el archivo especificado
+		//cambiar el archivo por variable a futuro, no prioritario de momento
 		string_append(&direccionIns, pathInstrucciones);
 		string_append(&direccionIns, "/");
 		string_append(&direccionIns,*file);
 		//abre el archivo en modo lectura
 		fileInstrucciones = fopen(direccionIns, "r");
-		char* lineaDeCodigo=calloc(101,sizeof(char));
+
 		if (fileInstrucciones == NULL)
 		    {
 			 printf("no hay insctrucciones o hubo error con el archivo");
@@ -19,19 +19,19 @@ t_list* cargar_instrucciones(char** file){
 		 	else
 		    {
 		 	    printf("\nEl contenido del archivo de prueba es \n");
-		 	    while (fgets(lineaDeCodigo,sizeof(lineaDeCodigo),fileInstrucciones )!=NULL)
-		 	    {
-		 			char* lineaTemporal=calloc(101,sizeof(char));
-		 	        string_append(&lineaTemporal, lineaDeCodigo);
-		 	    	list_add(listaInstrucciones,lineaTemporal);
-		 	    }
-		    }
-	    fclose(fileInstrucciones);
-		    char* primerComando=list_get(listaInstrucciones,0);
-		    printf("Línea %d: %s\n", 0, primerComando);
-			printf("\n tamaño de lista %d \n", list_size(listaInstrucciones));
 
-			free(lineaDeCodigo);
-			free(primerComando);
-		    return listaInstrucciones;
+			char* lineaDeCodigo=NULL;
+			size_t lineLength = 0;
+			while (getline(&lineaDeCodigo, &lineLength, fileInstrucciones) != -1) {
+				if (lineaDeCodigo[strlen(lineaDeCodigo) - 1] == '\n') {
+					lineaDeCodigo[strlen(lineaDeCodigo) - 1] = '\0';
+				}
+				char* lineaTemporal = strdup(lineaDeCodigo);
+				list_add(listaInstrucciones, lineaTemporal);
+			}
+				free(lineaDeCodigo);
+			}
+			fclose(fileInstrucciones);
+			printf("\n tamaño de lista %d \n", list_size(listaInstrucciones));
+			return listaInstrucciones;
 }
