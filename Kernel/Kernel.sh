f8e4fcdf5ebfd8d95f3c651033afac8dd4d375f0
juan9772 <juanpirinto@hotmail.com>
2023-11-18 03:33:12 +0000

    correcion de kernel.sh
    se agrego para que compile todos los archivos en los cuales dividimos el kernel

--- Kernel/Kernel.sh
+++ Kernel/Kernel.sh
@@ -3,8 +3,37 @@
 #
 echo 'compilando binario de Kernel'
 cd Debug
+
+echo 'Building file: ../src/Consola.c'
+echo 'Invoking: GCC C Compiler'
+gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/Consola.d" -MT"src/Consola.o" -o "src/Consola.o" "../src/Consola.c"
+echo 'echo "Finished building: ../src/Consola.c'
+
+echo 'Building file: ../src/Generales.c'
+echo 'Invoking: GCC C Compiler'
+gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/Generales.d" -MT"src/Generales.o" -o "src/Generales.o" "../src/Generales.c"
+echo 'Finished building: ../src/Generales.c'
+ 
+echo 'Building file: ../src/PlanificadorCorto.c'
+echo 'Invoking: GCC C Compiler'
+gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/PlanificadorCorto.d" -MT"src/PlanificadorCorto.o" -o "src/PlanificadorCorto.o" "../src/PlanificadorCorto.c"
+echo 'Finished building: ../src/PlanificadorCorto.c'
+ 
+echo 'Building file: ../src/PlanificadorLargo.c'
+echo 'Invoking: GCC C Compiler'
+gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/PlanificadorLargo.d" -MT"src/PlanificadorLargo.o" -o "src/PlanificadorLargo.o" "../src/PlanificadorLargo.c"
+echo 'Finished building: ../src/PlanificadorLargo.c'
+
+echo 'Building file: ../src/Kernel.c'
+echo 'Invoking: GCC C Compiler'
 gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/Kernel.d" -MT"src/Kernel.o" -o "src/Kernel.o" "../src/Kernel.c"
+echo 'Finished building: ../src/Kernel.c'
+
+echo 'Building target: Kernel'
+echo 'Invoking: GCC C Linker'
 gcc -o "Kernel" ./src/Consola.o ./src/Generales.o ./src/Kernel.o ./src/PlanificadorCorto.o ./src/PlanificadorLargo.o    -lcommons -lUtils -lreadline
-echo 'ejecutando binario de Kernel'
+echo 'Finished building target: Kernel'
+
 cd ..
+echo 'ejecutando binario de Kernel'
 valgrind -s ./Debug/Kernel
