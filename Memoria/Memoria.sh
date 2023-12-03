a46826e9b49f28f43e7134742c4d79676073c334
juan9772 <juanpirinto@hotmail.com>
2023-12-03 23:43:32 +0000

    Update Memoria.sh

--- Memoria/Memoria.sh
+++ Memoria/Memoria.sh
@@ -2,9 +2,26 @@
 #script de Memoria
 #
 cd Debug
-echo 'compilando binario de Memoria'
+echo 'Building file: ../src/MemoriaUsuario.c'
+echo 'Invoking: GCC C Compiler'
+gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/MemoriaUsuario.d" -MT"src/MemoriaUsuario.o" -o "src/MemoriaUsuario.o" "../src/MemoriaUsuario.c"
+echo 'echo "Finished building: ../src/MemoriaUsuario.c'
+
+echo 'Building file: ../src/MemoriaInstruciones.c'
+echo 'Invoking: GCC C Compiler'
+gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/MemoriaInstruciones.d" -MT"src/MemoriaInstruciones.o" -o "src/MemoriaInstruciones.o" "../src/MemoriaInstruciones.c"
+echo 'echo "Finished building: ../src/MemoriaInstruciones.c'
+
+echo 'Building file: ../src/MemoriaInstruciones.c'
+echo 'Invoking: GCC C Compiler'
 gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/Memoria.d" -MT"src/Memoria.o" -o "src/Memoria.o" "../src/Memoria.c"
+echo 'echo "Finished building: ../src/MemoriaInstruciones.c'
+
+echo 'Building target: Memoria'
+echo 'Invoking: GCC C Linker'
 gcc -o "Memoria" ./src/Memoria.o ./src/MemoriaInstruciones.o ./src/MemoriaUsuario.o    -lcommons -lUtils -lreadline
+echo 'Finished building target: Memoria'
+
 cd ..
 echo 'ejecutando binario de Memoria'
 valgrind -s ./Debug/Memoria
